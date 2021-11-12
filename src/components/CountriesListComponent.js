09c03a7ce319d29f812be7ab3adf75d3a4496805
AiltonRafael <ailtonrafaeldiasjr@gmail.com>
2021-11-12 18:38:12 -0300

    updated

--- src/components/CountriesListComponent.js
+++ src/components/CountriesListComponent.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box } from '@chakra-ui/react';
 import { Button } from '@chakra-ui/button';
 
 export default function CountriesListComponent(props) {
+    const [isToggle, setIsToggle] = useState(false)
+    function handleClick(){
+        setIsToggle(!isToggle)
+    }
+
     return (
         <Box
         h='100%'
         w='30vh'
         >
-            <Button> show/hide </Button>
-            <Box> 
+            <Button
+            onMouseOver={handleClick}
+            > 
+                show/hide 
+            </Button>
+
+            <Box
+            d={isToggle ? 'inline' : 'none'}
+            position='fixed'
+            top='140px'
+            left='0px'
+            right='50%'
+            > 
             {props.data.map((country) => {
                 return (
                     <Button
-                    w='100%'
+                    w='20%'
                     mt='1px'
                     _hover={{
                         bg: '100',
